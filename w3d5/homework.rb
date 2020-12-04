21f75ef96b13347ddf5841672a72e09c3291abfe
Chubbibanana <dongsoo.tchah@gmail.com>
2020-12-04 08:50:18 -0800

    Stack done.

--- w3d5/homework.rb
+++ w3d5/homework.rb
@@ -1,3 +1,17 @@
-def homework
+class Stack
+    def initialize(array = [])
+        @stack = array
+    end
 
+    def push(el)
+        @stack.push(el)
+    end
+
+    def pop
+        @stack.pop
+    end
+
+    def peek
+        @stack[-1]
+    end
 end
