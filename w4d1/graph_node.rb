c3ad231967d68b7d36057c9bda76fedfa99f7ff8
Chubbibanana <dongsoo.tchah@gmail.com>
2020-12-07 08:23:57 -0800

    Have to fix bfs-undefined method

--- w4d1/graph_node.rb
+++ w4d1/graph_node.rb
@@ -1,4 +1,5 @@
 class GraphNode
+    attr_accessor :neighbors
     def initialize(value)
         @value = value
         @neighbors = []
@@ -6,12 +7,32 @@ class GraphNode
     end
 
     def neighbors=(node_array)
-
+        node_array.each do |node|
+            @neighbors << node
+        end
     end
 
     def bfs(starting_node, target_value)
-
+        queue = [starting_node]
+        parsed = []
+        until queue.empty?
+            next_node = queue.shift
+            return next_node if next_node.value == target_value
+            add_to = next_node.children.select{|ele| !parsed.include?(ele)}
+            queue << add_to
+        end
+        nil
     end
-
-
 end
+
+a = GraphNode.new('a')
+b = GraphNode.new('b')
+c = GraphNode.new('c')
+d = GraphNode.new('d')
+e = GraphNode.new('e')
+f = GraphNode.new('f')
+a.neighbors = [b, c, e]
+c.neighbors = [b, d]
+e.neighbors = [a]
+f.neighbors = [e]
+bfs(a, 'b')
