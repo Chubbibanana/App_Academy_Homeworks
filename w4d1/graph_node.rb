65c052c0243cbcc0721ef7bbe15b27fd1823a34a
Chubbibanana <dongsoo.tchah@gmail.com>
2020-12-07 08:15:25 -0800

    started w4d1 homework

---
+++ w4d1/graph_node.rb
@@ -0,0 +1,17 @@
+class GraphNode
+    def initialize(value)
+        @value = value
+        @neighbors = []
+
+    end
+
+    def neighbors=(node_array)
+
+    end
+
+    def bfs(starting_node, target_value)
+
+    end
+
+
+end
