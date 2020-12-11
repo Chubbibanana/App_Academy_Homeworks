56c8ff10b87127d1ca7e8f659fc9742a13153209
Chubbibanana <dongsoo.tchah@gmail.com>
2020-12-10 19:54:13 -0800

    Done with all homeworks

--- w4d5/octopus_problems.rb
+++ w4d5/octopus_problems.rb
@@ -33,5 +33,25 @@ def merge_sort(array)
     merge(merge_sort(left), merge_sort(right))
 end
 
-array = ['fish', 'fiiish', 'fiiiiish', 'fiiiish', 'fffish', 'ffiiiiisshh', 'fsh', 'fiiiissshhhhhh']
-p dominant_octopus(array)
+def clever_octopus(array)
+    array.inject {|acc, ele| acc.length < ele.length ? ele : acc}
+end
+
+
+
+def slow_dance(dir, tiles_array)
+    tiles_array.each_with_index {|ele, i| return i if dir == ele}
+    nil
+end
+
+def fast_dance(dir, tiles_hash)
+    tiles_hash[dir]
+end
+
+tiles_array = ["up", "right-up", "right", "right-down", "down", "left-down", "left",  "left-up" ]
+hash = Hash.new{}
+tiles_array.each_with_index {|ele, i| hash[ele] = i}
+p slow_dance("up", tiles_array)
+p slow_dance("right-down", tiles_array)
+p fast_dance("up", hash)
+p fast_dance("right-down", hash)
