c5006e6344bc1a047395fa84c9bea00a19ec23b4
Chubbibanana <dongsoo.tchah@gmail.com>
2020-12-17 09:45:37 -0800

    Finished

--- w5d4/demo_project/app/models/house.rb
+++ w5d4/demo_project/app/models/house.rb
@@ -1,3 +1,9 @@
 class House < ApplicationRecord
+    validates :address, presence: true
 
+    has_many(:toys, {
+        primary_key: :id,
+        foreign_key: :house_id,
+        class_name: :Person
+    })
 end
